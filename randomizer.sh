e3598a9a3a35d05446b16debb219a600f9dafb85
sierraneu <85971509+sierraneu@users.noreply.github.com>
2021-11-10 12:50:58 -0500

    Update randomizer.sh

--- randomizer.sh
+++ randomizer.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-nuid=10#$1
+nuid=$((10#$1))
 sc1=$(( $nuid % 15))
 sc1=$(( 2 * $sc1))
-echo "Thanks $2 your 10 programs to work with  are:"
-for (( c=1; c<=10; c++ ))
-do 
-	increment=$(( 2 * $c)) 
-	code=$(($(($sc1 + $increment)) % 32))
-	if [[ $code -lt 10 ]]
-	then
-	echo "$c. code110$code.c"
-	else
-	echo "$c. code11$code.c"
-	fi
+echo -e "Thanks, your email is: $2, \nand your 9 programs to work with are:"
+for (( c=1; c<10; c++ ))
+do
+        increment=$(( 2 * $c))
+        code=$(($(($sc1 + $increment)) % 32))
+        if [[ $code -lt 10 ]]
+        then
+        echo "$c. code110$code.c"
+        else
+        echo "$c. code11$code.c"
+        fi
 done
+
